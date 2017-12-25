51edbddec845dc492996425ab246d8ce72db144f
Daniel Cotic <d.cotic26@gmail.com>
2017-12-25 19:10:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,5 +2,5 @@ function forLoop(array){
  for (var i = 0; i < 25; i++) {
  if(i === 1) {console.log( "I am 1 strange loop." )} else{
  console.log( "I am " + i + " strange loops.")}}
- return array + i
+ return array
 }
